1f1fca997c73999d772ebf5ede940f826da7e9ec
SoufianG <garaoui.soufian@gmail.com>
2025-05-26 08:03:04 +0200

    les animations
    ca bug pu hihi

--- Scripts/Player2/player_2.gd
+++ Scripts/Player2/player_2.gd
@@ -2,8 +2,10 @@ extends CharacterBody2D
 
 @onready var anim_tree = get_node("AnimationTree")
 
-const speed = 200
-var attacking = false
+const speed:int = 200
+var attacking:bool = false
+var dying:bool = false
+var health:int = 100
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
@@ -11,11 +13,11 @@ func _physics_process(delta):
 		attacking = true
 	
 	
-	if attacking == false: 
+	if (attacking == false) and (dying == false): 
 		var input_vector = Vector2 (
-			Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
-			Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-			).normalized()
+		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
+		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+		).normalized()
 		
 		velocity = input_vector * speed 
 		
@@ -28,9 +30,18 @@ func _physics_process(delta):
 		anim_tree.set("parameters/Attack/BlendSpace2D/blend_position", input_vector)
 		anim_tree.set("parameters/Walk/BlendSpace2D/blend_position", input_vector)
 		
+		hit(5)
+		print(health)
 		move_and_slide()
 
-
 func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
-	if "Attack" in anim_name:
+	if "attack" in anim_name:
 		attacking = false
+		
+func hit(damage):
+	health -= damage
+	if health <= 0 :
+		dying = true
+		anim_tree.get("parameters/playback").travel("Death")
+		await anim_tree.animation_finished
+		self.queue_free()
